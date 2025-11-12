018b36dc8f145f4f205e190e8072c64bac58e701
Sylvester <sylvester@tourcare.dk>
2025-11-12 10:13:53 +0100

    updated login

--- public/login.js
+++ public/login.js
@@ -1,27 +1,70 @@
-document.getElementById("loginBtn").addEventListener("click", async () => {
+const loginBtn = document.querySelector('#loginBtn');
+const statusText = document.querySelector('#status')
+
+const loginInputs = document.querySelectorAll("#username, #password");
+
+loginInputs.forEach(input => {
+    input.addEventListener("keydown", (event) => {
+        if (event.key === "Enter") {
+            event.preventDefault(); 
+            loginBtn.click();
+        }
+    });
+});
+
+// Din eksisterende login-funktion
+loginBtn.addEventListener("click", async () => {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
-    const status = document.getElementById("status");
 
     if (!username || !password) {
-        status.innerText = "Udfyld begge felter!";
-        return;
-    }
+        statusText.textContent = "Udfyld begge felter!"
+        loginBtn.style.display = 'none';
 
+        await setTimeout(() => {
+            loginBtn.style.display = ''
+            statusText.textContent = ""
+        }, 3000)
+        loginBtn.disabled = false
+        return
+    }
+    loginBtn.disabled = true
     try {
         const res = await fetch("/login", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ username, password })
         });
+
         const data = await res.json();
 
         if (res.ok) {
-            window.location.href = "/"; // Send til index
+            window.location.href = "/";
         } else {
-            status.innerText = "❌ " + data.error;
+            statusText.textContent = data.error;
+            loginBtn.style.display = 'none';
+
+            await setTimeout(() => {
+                loginBtn.style.display = ''
+                statusText.textContent = ""
+            }, 3000)
+            loginBtn.disabled = false
+            return
         }
+        
     } catch {
-        status.innerText = "❌ Netværksfejl";
+
+        statusText.textContent = "Serverfejl. Kontakt Sylle"
+        loginBtn.style.display = 'none';
+
+        await setTimeout(() => {
+            loginBtn.style.display = ''
+            statusText.textContent = ""
+        }, 3000)
+        loginBtn.disabled = false
+        return
+
+    } finally {
+        loginBtn.disabled = false;
     }
 });
